95970af62019566fba76371f0c1597c4303ec4c9
rahul <rkumar.jnvr@gmail.com>
2021-06-28 21:15:01 +0530

    change wow

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-// const MONGODB_URL='mongodb+srv://Rahul_user:rahulkumar@rahul.qsswr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 mongoose.connect(process.env.MONGODB_URL, {
     useFindAndModify: false,
     useNewUrlParser: true,
